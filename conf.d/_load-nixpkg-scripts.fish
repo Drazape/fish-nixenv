fad282174c37a52860ed7e1924175372001f3304
Drazape <Drazape@outlook.com>
2026-08-29 15:09:30 +0530

    feat(environment): restore script paths as-is
    Instead of manually computing the function/completion paths changes, and reverting them, simply save the changes, and restore them later.

--- conf.d/_load-nixpkg-scripts.fish
+++ conf.d/_load-nixpkg-scripts.fish
@@ -3,26 +3,17 @@ function _load-nixpkg-scripts --description='Automatically load a Nix pkg script
         for init_script in {$FISH_NIXPKG}/share/fish/vendor_conf.d/*
             source -- {$init_script} # source only accepts a single path
         end
+
+        set --global -- old_function_path {$fish_function_path}
         set --prepend -- fish_function_path {$FISH_NIXPKG}/share/fish/vendor_functions.d
+
+        set --global -- old_complete_path {$fish_complete_path}
         set --prepend -- fish_complete_path {$FISH_NIXPKG}/share/fish/vendor_completions.d
-        set --global -- fish_nixpkg_save {$FISH_NIXPKG}
     else
-        for component in functions completions
-            set --local -- fish_path
-            switch {$component}
-                case functions
-                    set -- fish_path fish_function_path
-                case completions
-                    set -- fish_path fish_complete_path
-            end
-            for component_path in {$$fish_path}
-                set --local -- path_filter
-                # doesn't use [1] index incase the path is later further modified
-                test {$component_path} != {$fish_nixpkg_save}/share/fish/vendor_{$component}.d &&
-                    set --append -- path_filter {$component_path}
-                set -- {$fish_path} {$path_filter}
-            end
-        end
-        set --erase --global -- fish_nixpkg_save
+        set -- fish_function_path {$old_function_path}
+        set --erase --global -- old_function_path
+
+        set -- fish_complete_path {$old_complete_path}
+        set --erase --global -- old_complete_path
     end
 end
