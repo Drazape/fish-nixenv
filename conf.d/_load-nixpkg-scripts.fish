b7cb2a6dc07d38326d78a3b73ce8e1ae9bda0b1e
Drazape <Drazape@outlook.com>
2026-08-30 16:05:04 +0530

    fix(vars): global: hide
    Hide internal global variables by prefixing them with a `_`

--- conf.d/_load-nixpkg-scripts.fish
+++ conf.d/_load-nixpkg-scripts.fish
@@ -4,16 +4,16 @@ function _load-nixpkg-scripts --description='Automatically load a Nix pkg script
             source -- {$init_script} # source only accepts a single path
         end
 
-        set --global -- old_function_path {$fish_function_path}
+        set --global -- _old_function_path {$fish_function_path}
         set --prepend -- fish_function_path {$FISH_NIXPKG}/share/fish/vendor_functions.d
 
-        set --global -- old_complete_path {$fish_complete_path}
+        set --global -- _old_complete_path {$fish_complete_path}
         set --prepend -- fish_complete_path {$FISH_NIXPKG}/share/fish/vendor_completions.d
     else
-        set -- fish_function_path {$old_function_path}
-        set --erase --global -- old_function_path
+        set -- fish_function_path {$_old_function_path}
+        set --erase --global -- _old_function_path
 
-        set -- fish_complete_path {$old_complete_path}
-        set --erase --global -- old_complete_path
+        set -- fish_complete_path {$_old_complete_path}
+        set --erase --global -- _old_complete_path
     end
 end
