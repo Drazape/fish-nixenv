b376b8276ba9b9874eb0dd50cc1d82e87627987e
Drazape <Drazape@outlook.com>
2026-09-01 16:51:19 +0530

    feat(hooks): rely on source and unloading variables
    Instead of relying on the parsed name of the package (that could be dynamic), use the sourced script directly for initialization.
    The specialized sourced script sets a global environment variable that is checked by the termination if it is needed. The termination script erases the variable once executed.
    
    This new structure should make the hooks easier to write, and also make the source code of the project less complicated.

--- conf.d/nixpkg-hooks.fish
+++ conf.d/nixpkg-hooks.fish
@@ -1,37 +1,15 @@
 function _run-nixpkg-hooks --description='Automatically manage a Nix pkg environment' --on-variable=FISH_{NIXPKG,DIRENV_HOOKS}
-    set --global -- _fish_current_nixpkg_name (string split --fields=2 --max=1 -- - {$FISH_NIXPKG}) # checked by `add` hooks
     if set --query --export --global -- FISH_DIRENV_HOOKS || set --query --export --global -- FISH_NIXPKG
         set --query --export --global -- FISH_DIRENV_HOOKS && source -- {$FISH_DIRENV_HOOKS}
         if set --query --global -- _nixpkg_fish_loaded
-            emit -- nixpkg-hook-remove
-            emit -- nixpkg-hook-add
+            emit -- direnv-hook-remove
+            _load-nixpkg-scripts_remove
+            _load-nixpkg-scripts_add
         else
-            emit -- nixpkg-hook-add
-            set --global -- _nixpkg_fish_loaded
+            _load-nixpkg-scripts_add
         end
     else
-        emit -- nixpkg-hook-remove
-        set --erase --global -- _nixpkg_fish_loaded
+        emit -- direnv-hook-remove
+        _load-nixpkg-scripts_remove
     end
-    set --global -- _fish_previous_nixpkg_name (string split --fields=2 --max=1 -- - {$FISH_NIXPKG}) # checked by `remove` hooks
-end
-
-# universal hooks
-function _load-nixpkg-scripts_add --description='Load a Fish Nixpkg into an environment' --on-event=nixpkg-hook-add
-    for init_script in {$FISH_NIXPKG}/share/fish/vendor_conf.d/*
-        source -- {$init_script} # source only accepts a single path
-    end
-
-    set --global -- _old_function_path {$fish_function_path}
-    set --prepend -- fish_function_path {$FISH_NIXPKG}/share/fish/vendor_functions.d
-
-    set --global -- _old_complete_path {$fish_complete_path}
-    set --prepend -- fish_complete_path {$FISH_NIXPKG}/share/fish/vendor_completions.d
-end
-function _load-nixpkg-scripts_remove --description='Erase a Fish Nixpkg from an environment' --on-event=nixpkg-hook-remove
-    set -- fish_function_path {$_old_function_path}
-    set --erase --global -- _old_function_path
-
-    set -- fish_complete_path {$_old_complete_path}
-    set --erase --global -- _old_complete_path
 end
