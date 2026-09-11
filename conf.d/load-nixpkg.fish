a0f2177ae2f8cd08254ebf8f54f547a84dba1646
Drazape <Drazape@outlook.com>
2026-09-11 16:13:10 +0530

    feat(arbitrary): run change on Nixpkg updates
    When a Nixpkg was updated, the arbitrary hooks' value would remain the same, not triggering it.
    Now, the arbitrary hooks for changes will be triggered even when only the Nixpkg updates.

--- conf.d/load-nixpkg.fish
+++ conf.d/load-nixpkg.fish
@@ -1,6 +1,9 @@
 function _load-nixpkg --description='Automatically manage a Nix pkg environment' --on-variable=FISH_NIXPKG
     if set --query --export --global -- FISH_NIXPKG
-        set --query --global -- _nixpkg_fish_loaded && _load-nixpkg_remove
+        if set --query --global -- _nixpkg_fish_loaded
+            _load-nixpkg_remove
+            _run-direnv-scripts # trigger change hooks
+        end
         _load-nixpkg_add
         set --global -- _nixpkg_fish_loaded
     else
