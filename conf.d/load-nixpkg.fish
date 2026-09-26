9344e9c1deac9e51b66aa1363c9929af29cbd024
Drazape <Drazape@outlook.com>
2026-09-26 12:19:00 +0530

    feat(log): output operation logs
    Notify the user about what operation is being performed

--- conf.d/load-nixpkg.fish
+++ conf.d/load-nixpkg.fish
@@ -1,12 +1,15 @@
 function _load-nixpkg --description='Automatically manage a Nix pkg environment' --on-variable=FISH_NIXPKG
     if set --query --export --global -- FISH_NIXPKG
         if set --query --global -- _nixpkg_fish_loaded
+            echo 'switching the Fish plugin currently loaded in the environment'
             _load-nixpkg_remove
             _run-direnv-scripts # trigger change hooks
         end
+        echo 'loading Fish plugin provided by the current repository'
         _load-nixpkg_add
         set --global -- _nixpkg_fish_loaded
     else
+        echo 'removing Fish plugin provided by the current repository'
         _load-nixpkg_remove
         set --erase --global -- _nixpkg_fish_loaded
     end
--- functions/run-direnv-scripts/trigger-remove.fish
+++ functions/run-direnv-scripts/trigger-remove.fish
@@ -1,5 +1,6 @@
 function _run-direnv-scripts_trigger-remove --description='Trigger remove hook once, if set'
     if set --query --global -- _fish_plugin_remover
+        echo 'removing the Fish plugin development environment'
         $_fish_plugin_remover
         set --erase --global -- _fish_plugin_remover
     end
