ee9e0d157fd8900a4ae0b20f21b1b41031218499
Drazape <Drazape@outlook.com>
2026-09-26 12:30:47 +0530

    feat(log): prepend output source
    Prepend the source of the output (the program and the hook) to the logs

--- conf.d/direnv-scripts.fish
+++ conf.d/direnv-scripts.fish
@@ -1,13 +1,13 @@
 function _run-direnv-scripts --description='Automatically manage direnv hooks' --on-variable=FISH_DIRENV_HOOKS
     if set --query --export --global -- FISH_DIRENV_HOOKS
         if set --query --global -- _fish_plugin_changer
-            echo 'switching Fish plugin repository'
+            echo 'fish-nixenv: arbitrary: switching Fish plugin repository'
             $_fish_plugin_changer
             set --erase --global -- _fish_plugin_changer
         else
             _run-direnv-scripts_trigger-remove
         end
-        echo 'creating Fish plugin development environment'
+        echo 'fish-nixenv: arbitrary: creating Fish plugin development environment'
         source -- {$FISH_DIRENV_HOOKS}
     else
         _run-direnv-scripts_trigger-remove
